d2e44a97752d17c2e329906af03184646a534332
Moritz Reis <me@moritzreis.dev>
2025-11-09 17:55:46 +0100

    feat: Implement Mireya API client services and update configuration options

--- src/Mireya.ApiClient/ServiceCollectionExtensions.cs
+++ src/Mireya.ApiClient/ServiceCollectionExtensions.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Net;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Mireya.ApiClient.Generated;
+using Mireya.ApiClient.Options;
+using Mireya.ApiClient.Services;
 
 namespace Mireya.ApiClient;
 
@@ -11,29 +17,28 @@ public static class ServiceCollectionExtensions
     /// Adds Mireya API client services to the dependency injection container
     /// </summary>
     /// <param name="services">The service collection</param>
-    /// <param name="configure">Configuration action for the client options</param>
     /// <returns>The service collection for chaining</returns>
     public static IServiceCollection AddMireyaApiClient(
-        this IServiceCollection services,
-        Action<MireyaApiClientOptions> configure)
+        this IServiceCollection services)
     {
+        var httpClientBuilder = services.AddHttpClient("UpdateServerApiClient");
+
+        httpClientBuilder
+            .ConfigureHttpClient((sp, httpClient) =>
+            {
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip");
+            });
+        
+        services.AddTransient<IMireyaApiClient, MireyaApiClient>(sp =>
+        {
+            var factory = sp.GetRequiredService<IHttpClientFactory>();
+            var opts = sp.GetRequiredService<IOptions<MireyaApiClientOptions>>();
+            return new MireyaApiClient(opts.Value.BaseUrl, factory.CreateClient("MireyaApiClient"));
+        });
+        
+        services.AddTransient<IMireyaService, MireyaService>();
         
         return services;
-    }
-
-    /// <summary>
-    /// Adds Mireya API client services with a simple base URL configuration
-    /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <param name="baseUrl">The base URL of the Mireya API</param>
-    /// <returns>The service collection for chaining</returns>
-    public static IServiceCollection AddMireyaApiClient(
-        this IServiceCollection services,
-        string baseUrl)
-    {
-        return services.AddMireyaApiClient(options =>
-        {
-            options.BaseUrl = baseUrl;
-        });
     }
 }
