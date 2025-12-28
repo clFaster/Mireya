1aae1b0b728261b34d4c866cb5ee1abf9681ffa5
Moritz Reis <me@moritzreis.dev>
2025-12-28 17:26:25 +0100

    chore: refactor

---
+++ src/Mireya.Client.Avalonia/Views/Components/NoContentDisplay.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Mireya.Client.Avalonia.Views.Components;
+
+public partial class NoContentDisplay : UserControl
+{
+    public NoContentDisplay()
+    {
+        InitializeComponent();
+    }
+}
