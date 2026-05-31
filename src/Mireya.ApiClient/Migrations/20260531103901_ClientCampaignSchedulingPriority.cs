e436860bd80e46d81a84ed4a50a09c62396faf9f
Moritz Reis <me@moritzreis.dev>
2026-05-31 12:40:27 +0200

    fix(Avalonia): Add missing migrations

---
+++ src/Mireya.ApiClient/Migrations/20260531103901_ClientCampaignSchedulingPriority.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mireya.ApiClient.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientCampaignSchedulingPriority : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDateUtc",
+                table: "Campaigns",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "Campaigns",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Campaigns",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDateUtc",
+                table: "Campaigns",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDateUtc",
+                table: "Campaigns");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "Campaigns");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Campaigns");
+
+            migrationBuilder.DropColumn(
+                name: "StartDateUtc",
+                table: "Campaigns");
+        }
+    }
+}
